07a933107d0898d8457d8e10fe746547ee0adeec
Vicente <33096355+Vicente015@users.noreply.github.com>
2023-05-30 12:49:42 +0100

    ✨ New color palettes & option select

--- _components/OptionsMenu.jsx
+++ _components/OptionsMenu.jsx
@@ -1,9 +1,14 @@
 export default function ({ comp }) {
+  const themes = ['olive', 'chestnut', 'cyan', 'blue', 'purple', 'cerise', 'coral', 'cocoa']
+
   return (
-    <div className='options' id='options'>
-      <select name='theme-select' id='theme-select'>
-        <option value='olive'>Olive</option>
-        <option value='chestnut'>Chestnut</option>
+    <div className='options' id='options' style={{ display: 'flex', justifyItems: 'center', alignContent: 'center', marginBottom: '1em' }}>
+      <select name='theme-select' id='theme-select' style={{ color: 'var(--heading-color)', padding: '6px 10px', backgroundColor: 'var(--bg-color)', border: '2px solid var(--zoom-bg-color)', borderRadius: '15px' }}>
+        {
+          themes.map((theme) => {
+            return <option key={theme} value={theme}>{theme.charAt(0).toUpperCase() + theme.substring(1)}</option>
+          })
+        }
       </select>
     </div>
   )
