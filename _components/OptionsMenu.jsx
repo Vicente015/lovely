92e31625ab9f9454e0240b0d221dd50e11a0bc28
Vicente <33096355+Vicente015@users.noreply.github.com>
2023-10-16 22:03:46 +0100

    feat: Improve styles for printing, move styles outside components

--- _components/OptionsMenu.jsx
+++ _components/OptionsMenu.jsx
@@ -2,8 +2,9 @@ export default function ({ comp }) {
   const themes = ['olive', 'chestnut', 'cyan', 'blue', 'purple', 'cerise', 'coral', 'cocoa']
 
   return (
-    <div className='options' id='options' style={{ display: 'flex', justifyItems: 'center', alignContent: 'center', marginBottom: '1em' }}>
-      <select name='theme-select' id='theme-select' style={{ color: 'var(--heading-color)', padding: '6px 10px', backgroundColor: 'var(--bg-color)', border: '2px solid var(--zoom-bg-color)', borderRadius: '15px' }}>
+    <div className='options' id='options' style={{ }}>
+      <label htmlFor="theme-select">Tema: </label>
+      <select name='theme-select' id='theme-select'>
         {
           themes.map((theme) => {
             return <option key={theme} value={theme}>{theme.charAt(0).toUpperCase() + theme.substring(1)}</option>
